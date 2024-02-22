f8096257c20b2d581c9d77667a51689e2350ae61
shikhar-batham <83720643+shikhar-batham@users.noreply.github.com>
2024-02-23 02:15:44 +0530

    login api call

--- app/src/main/java/com/example/insightlogfe/payload/RoleDto.java
+++ app/src/main/java/com/example/insightlogfe/payload/RoleDto.java
@@ -1,14 +1,6 @@
 package com.example.insightlogfe.payload;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
-@Getter
-@Setter
-@AllArgsConstructor
 public class RoleDto {
 
     private Integer id;
